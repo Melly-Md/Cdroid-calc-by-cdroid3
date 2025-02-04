bc8aea0ebe05e926af3512bbc8ec6387e78d4256
MellyFF <mellyyoung202013@gmail.com>
2025-02-04 17:12:19 +0100

    WEBCAM PHISHER

---
+++ Webcam-phishing.py
@@ -0,0 +1,15 @@
+import cv2
+
+capture = cv2.VideoCapture(0)
+
+while True:
+    _, frame = capture.read()
+    cv2.imshow("Capture from Webcam", frame)
+    
+    if cv2.waitKey(1) == 27:  # 27 is the ESC key
+        break
+
+capture.release()
+cv2.destroyAllWindows()
+
+
